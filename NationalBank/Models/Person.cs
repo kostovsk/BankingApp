5aba50c2c8d1fa59ca393a960073ca61b482c66e
Stoycho Kostov <stoichokostov@gmail.com>
2021-04-27 20:59:14 -0500

    Add project files.

---
+++ NationalBank/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NationalBank.Models
+{
+   public class Person
+   {
+      public string Name { get; set; }
+      public string Address { get; set; }
+      public string Phone { get; set; }
+      public string Email { get; set; }
+      public string Password { get; set; }
+
+      public Person(string name, string address, string phone, string email, string password)
+      {
+         this.Name = name;
+         this.Address = address;
+         this.Phone = phone;
+         this.Email = email;
+         this.Password = password;
+      }
+   }
+}
