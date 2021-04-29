75452129893d11c53cddd6c965901964dac8a5d4
Stoycho Kostov <stoichokostov@gmail.com>
2021-04-28 21:45:37 -0500

    redo

---
+++ FirstNationalBank/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FirstNationalBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstNationalBank.Data
+{
+   public class ApplicationDbContext : DbContext
+   {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+      public DbSet<BankAccount> BankAccounts { get; set; }
+   }
+}
