5ca4defbe02925b76945e273f62aba3dd4ec3696
Stoycho Kostov <stoichokostov@gmail.com>
2021-05-09 21:33:03 -0500

    views_update

---
+++ ForestNationalBank/Views/index.js
@@ -0,0 +1,17 @@
+﻿function openInputField() {
+    var loginForm = document.getElementById('login-form');
+    loginForm.setAttribute('type', 'hidden');
+
+    var createUserBtn = document.getElementById('create-btn');
+    createUserBtn.setAttribute('type', 'hidden');
+
+    document.getElementById('loginForm').style.display = 'block';
+}
+
+function redirectToProfilePage() {
+    document.location = 'Profile/ProfilePage';
+}
+
+function redirectToCreateAccount() {
+    document.location = 'Create/CreateAccount';
+}
