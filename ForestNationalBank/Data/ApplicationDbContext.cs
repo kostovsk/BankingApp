514a79d06c933d4ed95ced9d8a45aca4fe5eb3c8
Stoycho Kostov <stoichokostov@gmail.com>
2021-05-09 19:06:55 -0500

    models_data_controller_initial_set_up

---
+++ ForestNationalBank/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ForestNationalBank.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForestNationalBank.Data
+{
+   public class ApplicationDbContext : DbContext
+   {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+      {
+      }
+
+      public DbSet<BankAccount> BankAccounts { get; set; }
+      public DbSet<Person> Persons { get; set; }
+      public DbSet<Transaction> Transactions { get; set; }
+
+   }
+}
