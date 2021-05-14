464a8241427ccbaacea1e1331440f8aa2be8d5b8
Stoycho Kostov <stoichokostov@gmail.com>
2021-05-13 21:10:57 -0500

    create page wire up

---
+++ FirstNationalBank/wwwroot/js/create.js
@@ -0,0 +1,44 @@
+﻿const uri = 'https://localhost:44330/api/BankAccountsController';
+
+function addAccount() {
+    const name = document.getElementById('add-name');
+    const address = document.getElementById('add-address');
+    const account = document.getElementById('add-account');
+    const phone = document.getElementById('add-phone');
+    const email = document.getElementById('add-email');
+    const password = document.getElementById('add-password');
+    const balance = document.getElementById('add-balance');
+
+
+    const bankAccount = {
+        number: account.value.trim(),
+        balance: parseFloat(balance.value)
+    };
+
+    const person = {
+        name: name.value.trim(),
+        address: address.value.trim(),
+        phone: phone.value.trim(),
+        email: email.value.trim(),
+        password: password.value.trim()
+    }
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bankAccount)
+    })
+        .then(response => response.json())
+        .then(() => {
+            //getItems();
+            name.value = '';
+        })
+        .catch(error => console.error('Unable to add account.', error));
+}
+
+function test() {
+    alert("I am an alert box!");
+}
