b1ff17d9fe33b9769900c049af4224296e246cb9
Stoycho Kostov <stoichokostov@gmail.com>
2021-05-29 20:14:58 -0500

    added db migration with new db relations

--- FirstNationalBank/Migrations/20210530010358_AddModelsToDb.Designer.cs
+++ FirstNationalBank/Migrations/20210530010358_AddModelsToDb.Designer.cs
@@ -4,14 +4,16 @@ using FirstNationalBank.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FirstNationalBank.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210530010358_AddModelsToDb")]
+    partial class AddModelsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FirstNationalBank/Migrations/20210530010358_AddModelsToDb.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstNationalBank.Migrations
+{
+    public partial class AddModelsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Persons",
+                table: "Persons");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankAccounts",
+                table: "BankAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Persons");
+
+            migrationBuilder.RenameColumn(
+                name: "Acct_Id",
+                table: "Transactions",
+                newName: "BankAccountId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Transactions",
+                newName: "TransactionId");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Persons",
+                newName: "Zip");
+
+            migrationBuilder.RenameColumn(
+                name: "Acct_Id",
+                table: "Persons",
+                newName: "PersonId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "BankAccounts",
+                newName: "PersonId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonId",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MMN",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonId",
+                table: "BankAccounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BankAccountId",
+                table: "BankAccounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "BankAccounts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Persons",
+                table: "Persons",
+                column: "PersonId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankAccounts",
+                table: "BankAccounts",
+                column: "BankAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_BankAccountId",
+                table: "Transactions",
+                column: "BankAccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankAccounts_PersonId",
+                table: "BankAccounts",
+                column: "PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BankAccounts_Persons_PersonId",
+                table: "BankAccounts",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "PersonId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_BankAccounts_BankAccountId",
+                table: "Transactions",
+                column: "BankAccountId",
+                principalTable: "BankAccounts",
+                principalColumn: "BankAccountId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BankAccounts_Persons_PersonId",
+                table: "BankAccounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_BankAccounts_BankAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_BankAccountId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Persons",
+                table: "Persons");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankAccounts",
+                table: "BankAccounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BankAccounts_PersonId",
+                table: "BankAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "MMN",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "BankAccountId",
+                table: "BankAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "BankAccounts");
+
+            migrationBuilder.RenameColumn(
+                name: "BankAccountId",
+                table: "Transactions",
+                newName: "Acct_Id");
+
+            migrationBuilder.RenameColumn(
+                name: "TransactionId",
+                table: "Transactions",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "Zip",
+                table: "Persons",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonId",
+                table: "Persons",
+                newName: "Acct_Id");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonId",
+                table: "BankAccounts",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Acct_Id",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "BankAccounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Persons",
+                table: "Persons",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankAccounts",
+                table: "BankAccounts",
+                column: "Id");
+        }
+    }
+}
