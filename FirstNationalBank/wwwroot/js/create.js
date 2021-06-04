07120512e96fdde2ef4e6dad3b3c78d39029ad16
Stoycho Kostov <stoichokostov@gmail.com>
2021-06-03 22:21:42 -0500

    input validation on create page

--- FirstNationalBank/wwwroot/js/create.js
+++ FirstNationalBank/wwwroot/js/create.js
@@ -1,20 +1,41 @@
 ﻿const uri = 'https://localhost:44330/api/BankAccounts';
 
-function addAccount() {
-    const firstName = document.getElementById('add-first-name');
-    const lastName = document.getElementById('add-last-name');
-    const address = document.getElementById('add-address');
-    const city = document.getElementById('add-city');
-    const state = document.getElementById('add-state');
-    const zip = document.getElementById('add-zip');
-    const mmn = document.getElementById('add-mmn');
-    const phone = document.getElementById('add-phone');
-    const email = document.getElementById('add-email');
-    const password = document.getElementById('add-password');
-    const type = document.getElementById('add-type');
-    const account = document.getElementById('add-account');
-    const balance = document.getElementById('add-balance');
+const firstName = document.getElementById('add-first-name');
+const lastName = document.getElementById('add-last-name');
+const address = document.getElementById('add-address');
+const city = document.getElementById('add-city');
+const state = document.getElementById('add-state');
+const zip = document.getElementById('add-zip');
+const mmn = document.getElementById('add-mmn');
+const phone = document.getElementById('add-phone');
+const email = document.getElementById('add-email');
+const password = document.getElementById('add-password');
+const type = document.getElementById('add-type');
+const account = document.getElementById('add-account');
+const balance = document.getElementById('add-balance');
 
+var zipREGEX = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
+var phoneREGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+var emailREGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var passwordREGEX = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+
+function addAccount() {
+    if (!zipREGEX.test(zip.value)) {
+        alert("The Zip code is invalid");
+        return false;
+    }
+    if (!phoneREGEX.test(phone.value)) {
+        alert("The phone number is invalid");
+        return false;
+    }
+    if (!emailREGEX.test(email.value)) {
+        alert("The email address is invalid");
+        return false;
+    }
+    if (!passwordREGEX.test(password.value)) {
+        alert("The password needs to contain at least one number and one special character");
+        return false;
+    }
 
     const bankAccount = {
         number: account.value.trim(),
@@ -57,20 +78,6 @@ function addAccount() {
 }
 
 function redirectToProfilePage(data) {
-    const firstName = document.getElementById('add-first-name');
-    const lastName = document.getElementById('add-last-name');
-    const address = document.getElementById('add-address');
-    const city = document.getElementById('add-city');
-    const state = document.getElementById('add-state');
-    const zip = document.getElementById('add-zip');
-    const mmn = document.getElementById('add-mmn');
-    const phone = document.getElementById('add-phone');
-    const email = document.getElementById('add-email');
-    const password = document.getElementById('add-password');
-    const type = document.getElementById('add-type');
-    const account = document.getElementById('add-account');
-    const balance = document.getElementById('add-balance');
-
     firstName.value = '';
     lastName.value = '';
     address.value = '';
